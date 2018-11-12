7b7613f71f7f1c2233f7432464f1ca32975c811a
bkp506 <qualeetee114@gmail.com>
2018-11-12 12:03:54 +0530

    final DMS scripts DMS3

--- src/main/java/phfi/Scenarios/DMSDataUploadV3.java
+++ src/main/java/phfi/Scenarios/DMSDataUploadV3.java
@@ -1,10 +1,21 @@
 package phfi.Scenarios;
 
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.xmlbeans.impl.xb.xsdschema.Public;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -52,8 +63,12 @@ public class DMSDataUploadV3 {
 		GWait.Wait_GetElementById("btnconfiggo", 120).click();
 		HeadFileVerify();
 		CreateSubFolders();
-		HeadFileVerify();
-		CreateSubFolders();
+		DMSFolderCheck2();
+		DMSFolderCheck3Next();
+		CreateSubFolders2();
+		DMSFolderCheck2();
+		DMSFolderCheck3Next();
+		CreateSubFolders2();
 
 	}
 
@@ -66,7 +81,6 @@ public class DMSDataUploadV3 {
 
 		for (i1 = 1; i1 < RowCount; ) {
 
-				
 //				WebElement NextElmt = GWait.Wait_GetElementByXpath("//div[7]/div/div/div/div[3]/div[2]/div/ul/li[4]/a");
 				List<WebElement> FolderNameData = GlobalMethods.driver.findElements(By.xpath("//td[2]/a"));
 				for (WebElement webElement : FolderNameData) {
@@ -124,5 +138,158 @@ public class DMSDataUploadV3 {
 		}
 
 	}
+	
+	public void DMSFolderCheck2() throws Exception {
+		
+		FileInputStream fi = new FileInputStream(
+				System.getProperty("user.dir") + "/src/main/resources/DMSDataUpload.xls");
+		Workbook wb = Workbook.getWorkbook(fi);
+		Sheet st1 = wb.getSheet("CreateFolder");
+		int RowCount = st1.getRows();
+		
+		List<WebElement> FolderNameData = GlobalMethods.driver.findElements(By.xpath("//td[2]/a"));
+		
+//		for (int i = 0; i < FolderNameData.size(); i++) {
+			for (WebElement webElement : FolderNameData) {
+				String HeadFolder_Data = st1.getCell(2, i1).getContents();
+//				Thread.sleep(2500);
+				System.out.println("Folder Name: " + webElement.getText());
+				if (webElement.getText().equalsIgnoreCase(HeadFolder_Data)) {
+					Thread.sleep(1500);
+					webElement.click();
+					i1++;
+					break;
+					
+				}
+				
+			}
+			
+//		}
+		
+		}
+	
+	public void DMSFolderCheck3Next() throws Exception {
+		FileInputStream fi = new FileInputStream(
+				System.getProperty("user.dir") + "/src/main/resources/DMSDataUpload.xls");
+		Workbook wb = Workbook.getWorkbook(fi);
+		Sheet st1 = wb.getSheet("CreateFolder");
+		int RowCount = st1.getRows();
+		
+		try {
+			WebElement nextbtn = GlobalMethods.driver.findElement(By.xpath("//div[7]/div/div/div/div[3]/div[2]/div/ul/li[4]/a"));
+			GlobalMethods.scrollToElement(nextbtn);
+			nextbtn.click();
+			List<WebElement> FolderNameData1 = GlobalMethods.driver.findElements(By.xpath("//td[2]/a"));
+				for (WebElement webElement : FolderNameData1) {
+					String HeadFolder_Data = st1.getCell(2, i1).getContents();
+					System.out.println("Folder Name: " + webElement.getText());
+					if (webElement.getText().equalsIgnoreCase(HeadFolder_Data)) {
+						Thread.sleep(1500);
+						webElement.click();
+						i1++;
+						break;
+					}
+				}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.getMessage();
+		}
+	}
+	
+	public void CreateSubFolders2() throws Exception {
+		
+		FileInputStream fi = new FileInputStream(
+				System.getProperty("user.dir") + "/src/main/resources/DMSDataUpload.xls");
+		HSSFWorkbook wb = new HSSFWorkbook(fi);
+		 
+		HSSFSheet st1 = wb.getSheet("CreateFolder");
+		
+		Row row = st1.getRow(i);
+//		int RowCount1 = st1.getRows();
+//		for (i = i; i <= RowCount1; ) {
+		 
+	       int colNum = row.getLastCellNum();
+//			int ColomnCount = st1.getColumns();
+			System.out.println("Column ccount: "+colNum);
+			for (int j = 3; j < colNum; j++) {
+				
+//				cell = st1.getRow(i).getCell(j);
+
+				  String SubFolders_Data = row.getCell(j).getStringCellValue();
+				
+//				String SubFolders_Data = st1.getCell(j, i).getContents();
+				
+				Thread.sleep(1500);
+				GWait.Wait_GetElementById("btnShowCreateFolder", 120).click();
+
+				WebElement CreateFolder = GWait.Wait_GetElementById("txtAddNewfoldername", 120);
+				CreateFolder.sendKeys(SubFolders_Data);
+				GWait.Wait_GetElementById("btnconfiggo", 120).click();
+			}
+			i++;
+//			break;
+//		}
+		
+	}
+	public String BrowserFile_Data = null;
+	public void DMSDataUpload() throws Exception {
+
+		GlobalMethods.scrollToBottom();
+		FileInputStream fi = new FileInputStream(
+				System.getProperty("user.dir") + "/src/main/resources/UploadDocument.xls");
+		Workbook wb = Workbook.getWorkbook(fi);
+		Sheet r1 = wb.getSheet("UploadDocuments");
+		
+		int rowacount = r1.getRows();
+		for (int i = 1; i < rowacount - 1; i++) {
+			String FolderName_Data = r1.getCell(3, i).getContents();
+			
+			int colcount = r1.getColumns();
+			for (int j = 0; j < colcount; j++) {
+				String DocumentName_Data = r1.getCell(5, j).getContents();
+				String UploadVersion_Data = r1.getCell(6, j).getContents();
+//				BrowserFile_Data = r2.getCell(3, j).getContents();
+				String UploadFile_Data = r1.getCell(4, j).getContents();
+				String Comments_Data = r1.getCell(7, j).getContents();
+				
+				GWait.Wait_GetElementById("ctl00_ContentPlaceHolder1_anchAddDoc", 120).click();
+				
+				Thread.sleep(1000);
+				GWait.Wait_GetElementById("txtDocName", 120).sendKeys(DocumentName_Data);
+				GWait.Wait_GetElementById("txtVersion", 120).sendKeys(UploadVersion_Data);
+				GWait.Wait_GetElementByXpath(".//*[@id='rbtnVersion']/tbody/tr/td[2]/label").click();
+				GWait.Wait_GetElementByXpath(".//*[@id='rbtnAccess']/tbody/tr/td[2]/label").click();
+				GWait.Wait_GetElementByXpath(".//*[@id='rbtnLocation']/tbody/tr/td[1]/label").click();
+				GWait.Wait_GetElementById("fileUpload", 120).click();
+				
+				Thread.sleep(1500);
+				StringSelection selection = new StringSelection(UploadFile_Data);
+				java.awt.datatransfer.Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+				Thread.sleep(1000);
+				clipboard.setContents(selection, null);
+				Thread.sleep(1000);
+				Robot robot = new Robot();
+				robot.keyPress(KeyEvent.VK_CONTROL);
+				robot.keyPress(KeyEvent.VK_V);
+				robot.keyRelease(KeyEvent.VK_V);
+				robot.keyRelease(KeyEvent.VK_CONTROL);
+				Thread.sleep(1000);
+				robot.keyPress(KeyEvent.VK_ENTER);
+				robot.keyRelease(KeyEvent.VK_ENTER);
+				Thread.sleep(500);
+				GWait.Wait_GetElementById("txtComments", 120).sendKeys(Comments_Data);
+				Thread.sleep(1000);
+				GWait.Wait_GetElementById("ctl00_ContentPlaceHolder1_btnUpload", 120).click();
+				
+				Thread.sleep(4000);
+				GlobalMethods.isAlertPresent();
+
+			}
+			
+		}
+		
+		
+	}
+	
 
 }
